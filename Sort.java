b67a1bfbe85c2a8f1b86caac72f6f022c9e5463e
charlin2 <97994167+charlin2@users.noreply.github.com>
2022-04-11 01:22:00 -0400

    Add files via upload

--- Sort.java
+++ Sort.java
@@ -3,20 +3,22 @@ import java.util.Scanner;
 
 /**
  * CSDS233 Project 5: Sorting algorithms and benchmarking performance
+ * 
  * @author <i>Charlie Lin</i>
  */
 public class Sort {
     /**
      * Sorts an array in ascending order using insertion sort
+     * 
      * @param arr the array to be sorted
      */
     public void insertionSort(int[] arr) {
         for (int i = 1; i < arr.length; i++) {
-            if (arr[i] < arr[i-1]) {
+            if (arr[i] < arr[i - 1]) {
                 int toInsert = arr[i];
                 int j = i;
                 // iterate backwards, stopping when reaching the front of the list or when the current value is no longer less than the preceeding value
-                while (j > 0 && toInsert < arr[j-1]) {
+                while (j > 0 && toInsert < arr[j - 1]) {
                     arr[j] = arr[--j];
                 }
                 arr[j] = toInsert;
@@ -26,32 +28,35 @@ public class Sort {
 
     /**
      * Sorts an array in ascending order using quick-sort
+     * 
      * @param arr the array to be sorted
      */
     public void quickSort(int[] arr) {
-        qSort(arr, 0, arr.length-1);
+        qSort(arr, 0, arr.length - 1);
     }
 
     /**
      * Recursive helper method for quickSort
-     * @param arr the array to be sorted
+     * 
+     * @param arr   the array to be sorted
      * @param start the start of the range to be sorted
-     * @param end the end of the range to be sorted
+     * @param end   the end of the range to be sorted
      */
     private void qSort(int[] arr, int start, int end) {
         if (start < end) {
             int partition = partition(arr, start, end);
 
-            qSort(arr, start, partition-1);
-            qSort(arr, partition+1, end);
+            qSort(arr, start, partition - 1);
+            qSort(arr, partition + 1, end);
         }
     }
 
     /**
      * Helper method for qSort/quickSort
-     * @param arr the array to be sorted
+     * 
+     * @param arr   the array to be sorted
      * @param start the start of the range to be sorted
-     * @param end the end of the range to be sorted
+     * @param end   the end of the range to be sorted
      * @return the index of the partition
      */
     private int partition(int[] arr, int start, int end) {
@@ -80,6 +85,7 @@ public class Sort {
 
     /**
      * Sorts an array in ascending order using merge-sort
+     * 
      * @param arr the array to be sorted
      */
     public void mergeSort(int[] arr) {
@@ -88,26 +94,27 @@ public class Sort {
 
     /**
      * Recursive helper method for mergeSort, splits array and merges the sorted arrays
-     * @param arr the array to be sorted
+     * 
+     * @param arr   the array to be sorted
      * @param start the start index of the array
-     * @param end the end index of the array
+     * @param end   the end index of the array
      */
     private void mSort(int[] arr, int start, int end) {
         // Base case: array length is 1
         if (arr.length > 1) {
-            int middle = (end + start)/2;
+            int middle = (end + start) / 2;
 
             // construct left and right subarrays
             int[] leftArr = new int[middle];
-            int[] rightArr = new int[end-middle];
+            int[] rightArr = new int[end - middle];
 
             for (int i = 0; i < leftArr.length; i++)
                 leftArr[i] = arr[i];
 
-            for (int i = 0; i< rightArr.length; i++)
+            for (int i = 0; i < rightArr.length; i++)
                 rightArr[i] = arr[middle + i];
 
-            // recursively sort the subarrays 
+            // recursively sort the subarrays
             mSort(leftArr, 0, leftArr.length);
             mSort(rightArr, 0, rightArr.length);
 
@@ -117,44 +124,49 @@ public class Sort {
 
     /**
      * Merges two subarrays in sorted order
-     * @param arr the array to be sorted
-     * @param leftArr left subarray
+     * 
+     * @param arr      the array to be sorted
+     * @param leftArr  left subarray
      * @param rightArr right subarray
      */
     private static void merge(int[] arr, int[] leftArr, int[] rightArr) {
-		int leftCount = 0, rightCount = 0, arrCount = 0;
+        int leftCount = 0, rightCount = 0, arrCount = 0;
         // replace values in main array with sorted values from left and right subarrays
-		while (leftCount < leftArr.length && rightCount < rightArr.length) {
-			if (leftArr[leftCount] < rightArr[rightCount])
-				arr[arrCount++] = leftArr[leftCount++];
-			else
-				arr[arrCount++] = rightArr[rightCount++];
-		}
+        while (leftCount < leftArr.length && rightCount < rightArr.length) {
+            if (leftArr[leftCount] < rightArr[rightCount])
+                arr[arrCount++] = leftArr[leftCount++];
+            else
+                arr[arrCount++] = rightArr[rightCount++];
+        }
         // finish off the sorting
-		while (leftCount < leftArr.length)
-			arr[arrCount++] = leftArr[leftCount++];
-		while (rightCount < rightArr.length)
-			arr[arrCount++] = rightArr[rightCount++];
-	}
+        while (leftCount < leftArr.length)
+            arr[arrCount++] = leftArr[leftCount++];
+        while (rightCount < rightArr.length)
+            arr[arrCount++] = rightArr[rightCount++];
+    }
 
     /**
      * Generates a random array of <i>n</i> integers with values between <i>a</i> and <i>b</i>
+     * 
      * @param n the size of the array
      * @param a the lower bound for the potential values
      * @param b the upper bound for the potential values
      * @return a random array of size <i>n</i>
      */
     public int[] randomArray(int n, int a, int b) {
-        if (b < a || n <= 0) return new int[0];
+        if (b < a || n <= 0)
+            return new int[0];
         int[] randArr = new int[n];
+        // assign random values to each array index
         for (int i = 0; i < n; i++) {
-            randArr[i] = (int)(Math.random()*(b-a+1) + a);
+            randArr[i] = (int) (Math.random() * (b - a + 1) + a);
         }
         return randArr;
     }
 
     /**
      * Sorts an array in ascending order using bubble sort
+     * 
      * @param arr the array to be sorted
      */
     public void bubbleSort(int[] arr) {
@@ -163,10 +175,10 @@ public class Sort {
             int j = 1;
             // swap adjacent elements until largest value is at the end of the array
             while (j < i) {
-                if (arr[j] < arr[j-1]) {
+                if (arr[j] < arr[j - 1]) {
                     int temp = arr[j];
-                    arr[j] = arr[j-1];
-                    arr[j-1] = temp;
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
                 }
                 j++;
             }
@@ -185,7 +197,7 @@ public class Sort {
         for (int i = 0; i < 500; i++)
             partiallySortedArray[i] = i;
         for (int i = 500; i < partiallySortedArray.length; i++)
-            partiallySortedArray[i] = (int)(Math.random()*1000);
+            partiallySortedArray[i] = (int) (Math.random() * 1000);
 
         /** Insertion Sort */
         double totalTime = 0;
@@ -199,19 +211,19 @@ public class Sort {
         double start = System.nanoTime();
         sort.insertionSort(partiallySortedArray);
         double end = System.nanoTime();
-        System.out.println("InsertionSort for random arrays: " + totalTime/5 + " nanoseconds");
-        System.out.println("InsertionSort for partially sorted array: " + (end-start) + " nanoseconds");
+        System.out.println("InsertionSort for random arrays: " + totalTime / 5 + " nanoseconds");
+        System.out.println("InsertionSort for partially sorted array: " + (end - start) + " nanoseconds");
         start = System.nanoTime();
         sort.insertionSort(partiallySortedArray);
         end = System.nanoTime();
-        System.out.println("InsertionSort for sorted array: " + (end-start) + " nanoseconds\n");
+        System.out.println("InsertionSort for sorted array: " + (end - start) + " nanoseconds\n");
 
         /** Quick Sort */
         partiallySortedArray = new int[1000];
         for (int i = 0; i < 500; i++)
             partiallySortedArray[i] = i;
         for (int i = 500; i < partiallySortedArray.length; i++)
-            partiallySortedArray[i] = (int)(Math.random()*1000);
+            partiallySortedArray[i] = (int) (Math.random() * 1000);
         totalTime = 0;
         for (int i = 0; i < 6; i++) {
             int[] random = sort.randomArray(arrSize, 0, 1000);
@@ -223,19 +235,19 @@ public class Sort {
         start = System.nanoTime();
         sort.quickSort(partiallySortedArray);
         end = System.nanoTime();
-        System.out.println("QuickSort for random arrays: " + totalTime/5 + " nanoseconds");
-        System.out.println("QuickSort for partially sorted array: " + (end-start) + " nanoseconds");
+        System.out.println("QuickSort for random arrays: " + totalTime / 5 + " nanoseconds");
+        System.out.println("QuickSort for partially sorted array: " + (end - start) + " nanoseconds");
         start = System.nanoTime();
         sort.quickSort(partiallySortedArray);
         end = System.nanoTime();
-        System.out.println("QuickSort for sorted array: " + (end-start) + " nanoseconds\n");
-        
+        System.out.println("QuickSort for sorted array: " + (end - start) + " nanoseconds\n");
+
         /** Merge Sort */
         partiallySortedArray = new int[1000];
         for (int i = 0; i < 500; i++)
             partiallySortedArray[i] = i;
         for (int i = 500; i < partiallySortedArray.length; i++)
-            partiallySortedArray[i] = (int)(Math.random()*1000);
+            partiallySortedArray[i] = (int) (Math.random() * 1000);
         totalTime = 0;
         for (int i = 0; i < 6; i++) {
             int[] random = sort.randomArray(arrSize, 0, 1000);
@@ -247,19 +259,19 @@ public class Sort {
         start = System.nanoTime();
         sort.mergeSort(partiallySortedArray);
         end = System.nanoTime();
-        System.out.println("MergeSort for random arrays: " + totalTime/5 + " nanoseconds");
-        System.out.println("MergeSort for partially sorted array: " + (end-start) + " nanoseconds");
+        System.out.println("MergeSort for random arrays: " + totalTime / 5 + " nanoseconds");
+        System.out.println("MergeSort for partially sorted array: " + (end - start) + " nanoseconds");
         start = System.nanoTime();
         sort.mergeSort(partiallySortedArray);
         end = System.nanoTime();
-        System.out.println("MergeSort for sorted array: " + (end-start) + " nanoseconds\n");
+        System.out.println("MergeSort for sorted array: " + (end - start) + " nanoseconds\n");
 
         /** Arrays Sort */
         partiallySortedArray = new int[1000];
         for (int i = 0; i < 500; i++)
             partiallySortedArray[i] = i;
         for (int i = 500; i < partiallySortedArray.length; i++)
-            partiallySortedArray[i] = (int)(Math.random()*1000);
+            partiallySortedArray[i] = (int) (Math.random() * 1000);
         totalTime = 0;
         for (int i = 0; i < 6; i++) {
             int[] random = sort.randomArray(arrSize, 0, 1000);
@@ -271,19 +283,19 @@ public class Sort {
         start = System.nanoTime();
         Arrays.sort(partiallySortedArray);
         end = System.nanoTime();
-        System.out.println("Arrays Sort for random arrays: " + totalTime/5 + " nanoseconds");
-        System.out.println("Arrays Sort for partially sorted array: " + (end-start) + " nanoseconds");
+        System.out.println("Arrays Sort for random arrays: " + totalTime / 5 + " nanoseconds");
+        System.out.println("Arrays Sort for partially sorted array: " + (end - start) + " nanoseconds");
         start = System.nanoTime();
         Arrays.sort(partiallySortedArray);
         end = System.nanoTime();
-        System.out.println("Arrays Sort for sorted array: " + (end-start) + " nanoseconds\n");
+        System.out.println("Arrays Sort for sorted array: " + (end - start) + " nanoseconds\n");
 
         /** Bubble Sort */
         partiallySortedArray = new int[1000];
         for (int i = 0; i < 500; i++)
             partiallySortedArray[i] = i;
         for (int i = 500; i < partiallySortedArray.length; i++)
-            partiallySortedArray[i] = (int)(Math.random()*1000);
+            partiallySortedArray[i] = (int) (Math.random() * 1000);
         totalTime = 0;
         for (int i = 0; i < 6; i++) {
             int[] random = sort.randomArray(arrSize, 0, 1000);
@@ -295,12 +307,12 @@ public class Sort {
         start = System.nanoTime();
         sort.bubbleSort(partiallySortedArray);
         end = System.nanoTime();
-        System.out.println("BubbleSort for random arrays: " + totalTime/5 + " nanoseconds");
-        System.out.println("BubbleSort for partially sorted array: " + (end-start) + " nanoseconds");
+        System.out.println("BubbleSort for random arrays: " + totalTime / 5 + " nanoseconds");
+        System.out.println("BubbleSort for partially sorted array: " + (end - start) + " nanoseconds");
         start = System.nanoTime();
         sort.bubbleSort(partiallySortedArray);
         end = System.nanoTime();
-        System.out.println("BubbleSort for sorted array: " + (end-start) + " nanoseconds\n");
+        System.out.println("BubbleSort for sorted array: " + (end - start) + " nanoseconds\n");
 
     }
 }
--- SortTest.java
+++ SortTest.java
@@ -1,49 +1,120 @@
 import java.util.Arrays;
-
 import org.junit.Assert;
 import org.junit.Test;
 
 public class SortTest {
-    int[] array = {3, 1, 4, 12, 24, 23, 12, 30, 10, 12, 11, 5, 25, 25, 21};
     Sort sort = new Sort();
 
     @Test
     public void testInsertionSort() {
-        int[] sortArray = array;
+        // general sorting
+        int[] sortArray = {3, 1, 4, 12, 24, 23, 12, 30, 10, 12, 11, 5, 25, 25, 21};
         sort.insertionSort(sortArray);
         Assert.assertEquals("[1, 3, 4, 5, 10, 11, 12, 12, 12, 21, 23, 24, 25, 25, 30]", Arrays.toString(sortArray));
+
+        // sort on array of size one
+        sortArray = new int[1];
+        sortArray[0] = 1;
+        sort.insertionSort(sortArray);
+        Assert.assertEquals("[1]", Arrays.toString(sortArray));
+
+        // sort on empty array
         sortArray = new int[0];
         sort.insertionSort(sortArray);
         Assert.assertEquals("[]", Arrays.toString(sortArray));
+
+        // sort on null input
+        sortArray = null;
+        try {
+            sort.insertionSort(sortArray);
+        }
+        catch (NullPointerException e) {
+            Assert.assertTrue(true);
+        }
+        
     }
 
     @Test
     public void testQuickSort() {
-        int[] sortArray = array;
+        // general sorting
+        int[] sortArray = {3, 1, 4, 12, 24, 23, 12, 30, 10, 12, 11, 5, 25, 25, 21};
         sort.quickSort(sortArray);
         Assert.assertEquals("[1, 3, 4, 5, 10, 11, 12, 12, 12, 21, 23, 24, 25, 25, 30]", Arrays.toString(sortArray));
+
+        // sort on array of size one
+        sortArray = new int[1];
+        sortArray[0] = 1;
+        sort.quickSort(sortArray);
+        Assert.assertEquals("[1]", Arrays.toString(sortArray));
+
+        // sort on empty array
         sortArray = new int[0];
         sort.quickSort(sortArray);
         Assert.assertEquals("[]", Arrays.toString(sortArray));
+
+        // sort on null input
+        sortArray = null;
+        try {
+            sort.quickSort(sortArray);
+        }
+        catch (NullPointerException e) {
+            Assert.assertTrue(true);
+        }
     }
 
     @Test
     public void testMergeSort() {
-        int[] sortArray = array;
+        // general sorting
+        int[] sortArray = {3, 1, 4, 12, 24, 23, 12, 30, 10, 12, 11, 5, 25, 25, 21};
         sort.mergeSort(sortArray);
         Assert.assertEquals("[1, 3, 4, 5, 10, 11, 12, 12, 12, 21, 23, 24, 25, 25, 30]", Arrays.toString(sortArray));
+
+        // sort on array of size one
+        sortArray = new int[1];
+        sortArray[0] = 1;
+        sort.mergeSort(sortArray);
+        Assert.assertEquals("[1]", Arrays.toString(sortArray));
+
+        // sort on empty array
         sortArray = new int[0];
         sort.mergeSort(sortArray);
         Assert.assertEquals("[]", Arrays.toString(sortArray));
+        
+        // sort on null input
+        sortArray = null;
+        try {
+            sort.mergeSort(sortArray);
+        }
+        catch (NullPointerException e) {
+            Assert.assertTrue(true);
+        }
     }
 
     @Test
     public void testBubbleSort() {
-        int[] sortArray = array;
+        // general sorting
+        int[] sortArray = {3, 1, 4, 12, 24, 23, 12, 30, 10, 12, 11, 5, 25, 25, 21};
         sort.bubbleSort(sortArray);
         Assert.assertEquals("[1, 3, 4, 5, 10, 11, 12, 12, 12, 21, 23, 24, 25, 25, 30]", Arrays.toString(sortArray));
+
+        // sort on array of size one
+        sortArray = new int[1];
+        sortArray[0] = 1;
+        sort.bubbleSort(sortArray);
+        Assert.assertEquals("[1]", Arrays.toString(sortArray));
+
+        // sort on empty array
         sortArray = new int[0];
         sort.bubbleSort(sortArray);
         Assert.assertEquals("[]", Arrays.toString(sortArray));
+        
+        // sort on null input
+        sortArray = null;
+        try {
+            sort.bubbleSort(sortArray);
+        }
+        catch (NullPointerException e) {
+            Assert.assertTrue(true);
+        }
     }
 }
